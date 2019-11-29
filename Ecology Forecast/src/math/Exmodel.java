ee6ffe1708fe74fe74658cb1d91878450f192c61
admin <admin@DESKTOP-7KFE87L>
2019-11-29 06:28:16 -0800

    exmodel template

--- Ecology Forecast/src/math/Exmodel.java
+++ Ecology Forecast/src/math/Exmodel.java
@@ -1,5 +1,46 @@
 package math;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exmodel {
+	
+	private int newpop;
+	private int oldpop;
+	private double growth;
+	private double death;
+	private double densitygrowth;
+	private double densitydeath;
+	private int timeperiode;
+	
+	public int calculate(int oldpop, double growth, double death, double densitygrowth, double densitydeath)
+	{
+		int something = oldpop;
+		// IMPLEMENT THE CALCULATION
+		
+		return something;
+	}
+	
+	//CACulate for a periode of time
+	public List longcalculation(int time)
+	{
+		//RECURSIVE FUNCTION BASED ON TIME
+		
+		List a = new ArrayList(time);
+		
+		
+		
+	
+		
+		
+		return a;
+	}
+	
+	public static void Main(String argz)
+	{
+		Exmodel a = new Exmodel();
+		a.calculate(oldpop, growth, death, densitygrowth, densitydeath);
+		
+	}
 
 }
