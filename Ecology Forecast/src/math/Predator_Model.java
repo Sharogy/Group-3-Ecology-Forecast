4723ab23525339f50c83b0bb77800bb822a33b87
Shar <Shar@DESKTOP-BNPA2DR.home>
2019-12-19 00:04:30 +0100

    version 2.5a

--- Ecology Forecast/src/math/Predator_Model.java
+++ Ecology Forecast/src/math/Predator_Model.java
@@ -6,10 +6,44 @@ import model.Animal;
 import interfaces.imodel;
 
 public class Predator_Model {
+	private List<Animal> animallist;
+	private Animal ani;
+	private int timeperiod;
+	private boolean grassmode;
+	private imodel im;
+	private double predgrowth;
+	private double preddeath;
+	private int oldpredpop;
+	private int newpredpop;
 	
-	public List<Integer> calculate(List<Animal> animallist, Animal ani, int timeperiod) {
-		// TODO Auto-generated method stub
+	public int precalc (Animal ani, int timeperiod, boolean grassmode, imodel im) 
+	{
+		//calculates the population for 1 animal for 1 timestep
+		
+		return 0;		
+	}
+	
+	
+	public List<Integer> calculate(List<Animal> animallist, Animal ani, int timeperiod, boolean grassmode, imodel im) {
+		// calculates the population for 1 animal for N timesteps, returns a List<Integer>, see Exponential/Logistic model for example
+		
+		this.animallist = animallist;
+		this.ani = ani;
+		this.timeperiod = timeperiod;
+		this.grassmode = grassmode;
+		this.im = im;
+		
 		return null;
+	}
+	
+	public int getPredPop(int pop)
+	//calculates the population change for predators, assume growth/death rate of predator is known.
+	{
+		this.oldpredpop = pop;
+		//formula here
+		newpredpop = 0;
+		
+		return newpredpop;
 	}
 
 }
